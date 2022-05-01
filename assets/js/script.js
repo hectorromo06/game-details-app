3f822e23c1dcba4f08b4e09e7eef0a8df13c259c
analizajb <96861913+analizajb@users.noreply.github.com>
2022-05-01 00:31:32 -0700

    adding search handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
+console.log("JS test");
+
 // create array to hold searches for saving
 var search = [];
+
+var searchHandler = function (event) {
+    event.preventDefault();
+    var searchInput = document.querySelector("input[name='search']").value;
+    //var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+    // check if inputs are empty (validate)
+    if (!searchInput) {
+      alert("You forgot to type in here!"); // HOW TO MAKE THIS A MODAL?
+      return false;
+    }
+  
+    // reset form fields for next search to be entered
+    document.querySelector("input[name='search']").value = "";
+};
 
 var saveSearch = function () {
     localStorage.setItem("search", JSON.stringify(search));  
 }
 
-var loadsearch = function() {
+var loadSearch = function() {
     var savedSearch = localStorage.getItem("search");
     // if there are no tasks, set tasks to an empty array and return out of the function
     if (!savedSearch) {
@@ -22,5 +39,17 @@ var loadsearch = function() {
       // pass each task object into the `createTaskEl()` function
       createTaskEl(savedSearch[i]);
     }
-  };
-  
+};
+
+
+// create delete button
+var deleteButtonEl = document.createElement("button");
+     deleteButtonEl.textContent = "Delete";
+     deleteButtonEl.className = "btn delete-btn";
+     deleteButtonEl.setAttribute("data-task-id", taskId);
+     actionContainerEl.appendChild(deleteButtonEl);
+
+;
+
+
+loadSearch();
